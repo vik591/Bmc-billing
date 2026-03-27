51fd955150af5c220870a1cd9799439c5cd3c2ce
vik591 <vikii.vd@gmail.com>
2026-03-27 22:00:58 +0530

    Update InvoicePage.js

--- frontend/src/pages/InvoicePage.js
+++ frontend/src/pages/InvoicePage.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect, useRef } from 'react';
 import { useParams } from 'react-router-dom';
-import { productBillsAPI, settingsAPI } from '../lib/apiservice';
+import { productBillsAPI, settingsAPI } from '../lib/apiService';
 import { Button } from '../components/ui/button';
 import { toast } from 'sonner';
 import jsPDF from 'jspdf';
