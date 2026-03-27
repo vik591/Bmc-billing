94b10a9df31a4142f56a306e1e2e5a664832d3ea
vik591 <vikii.vd@gmail.com>
2026-03-27 15:07:50 +0530

    Update api.js

--- frontend/src/lib/api.js
+++ frontend/src/lib/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = process.env.REACT_APP_BACKEND_URL + '/api';
+const API_URL = import.meta.env.VITE_API_URL + '/api';
 
 const api = axios.create({
   baseURL: API_URL,
