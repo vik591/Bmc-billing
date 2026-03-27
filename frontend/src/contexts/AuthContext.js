73168bdc9662ac87bd323665ab84f0285b39b9a4
vik591 <vikii.vd@gmail.com>
2026-03-27 22:06:38 +0530

    Update AuthContext.js

--- frontend/src/contexts/AuthContext.js
+++ frontend/src/contexts/AuthContext.js
@@ -1,5 +1,5 @@
 import React, { createContext, useState, useContext, useEffect } from 'react';
-import { authAPI } from '../lib/apiservice';
+import { authAPI } from '../lib/apiService';
 
 const AuthContext = createContext(null);
 
