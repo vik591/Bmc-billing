25410145b81072084950d73dea326185326ddbdb
vik591 <vikii.vd@gmail.com>
2026-03-27 16:54:29 +0530

    Update api.js fix env variable for CRA
    fix env variable for CRA

--- frontend/src/lib/api.js
+++ frontend/src/lib/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 // correct for VITE
-const API_URL = import.meta.env.VITE_API_URL + '/api';
+const API_URL = process.env.REACT_APP_API_URL + '/api';
 
 const api = axios.create({
   baseURL: API_URL,
