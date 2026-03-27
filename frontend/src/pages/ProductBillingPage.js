2e1aeac7264e7b58f4206e228e7bf350868816ec
vik591 <vikii.vd@gmail.com>
2026-03-27 21:56:51 +0530

    Update ProductBillingPage.js

--- frontend/src/pages/ProductBillingPage.js
+++ frontend/src/pages/ProductBillingPage.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { productsAPI, productBillsAPI } from '../lib/api';
+import { productsAPI, productBillsAPI } from '../lib/apiservice';
 import { Button } from '../components/ui/button';
 import { Input } from '../components/ui/input';
 import { Label } from '../components/ui/label';
