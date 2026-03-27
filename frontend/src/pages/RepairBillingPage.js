b3b44561b246eeef38891ccab2663207512fc628
vik591 <vikii.vd@gmail.com>
2026-03-27 21:53:04 +0530

    Update RepairBillingPage.js

--- frontend/src/pages/RepairBillingPage.js
+++ frontend/src/pages/RepairBillingPage.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { repairBillsAPI } from '../lib/api';
+import { repairBillsAPI } from '../lib/apiservice';
 import { Button } from '../components/ui/button';
 import { Input } from '../components/ui/input';
 import { Label } from '../components/ui/label';
